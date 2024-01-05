4381c06b95089e10f465c6b3935fa9059a8e309e
John <sixtysecondrevit@gmail.com>
2024-01-04 17:52:32 -0700

    Enabled XML doc file

--- src/ZeroTouchUnraveled/MyNodes/MyFirstNode.cs
+++ src/ZeroTouchUnraveled/MyNodes/MyFirstNode.cs
@@ -8,10 +8,12 @@ namespace ZeroTouchUnraveled
 {
     public class MyFirstNode
     {
+        //this hides the overall class as a node 
         private MyFirstNode() { }
 
         public static string HelloWorld()
         {
+            //returns one output of hello world
             return "Hello world";
         }
     }
