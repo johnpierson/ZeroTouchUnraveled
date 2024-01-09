4106a82f9b44fabfdc1b7b5209cd46ebba2701b9
John <sixtysecondrevit@gmail.com>
2024-01-09 16:56:23 -0700

    Make our sheet node return the placed views.

--- src/ZeroTouchUnraveled/Revit/Views/Sheet.cs
+++ src/ZeroTouchUnraveled/Revit/Views/Sheet.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.DB;
+using Revit.Elements;
 using Revit.Elements.Views;
+using RevitServices.Persistence;
 
 
 
@@ -22,14 +24,31 @@ namespace ZeroTouchUnraveled.Revit.Views.Sheet
         /// </summary>
         /// <param name="sheet"></param>
         /// <returns></returns>
-        public static int GetAllPlacedViews(global::Revit.Elements.Views.Sheet sheet)
+        public static List<global::Revit.Elements.Views.View> GetAllPlacedViews(global::Revit.Elements.Views.Sheet sheet)
         {
-            //this casts the Dynamo Revit Sheet to a Revit.DB ViewSheet
+            Autodesk.Revit.DB.Document doc = DocumentManager.Instance.CurrentDBDocument;
+
+            //this casts the Dynamo Revit Sheet to a Autodesk.Revit.DB.ViewSheet
             ViewSheet viewSheet = sheet.InternalElement as ViewSheet;
 
-            var placedViews = viewSheet.GetAllPlacedViews();
+            //use the Revit API method on the Autodesk.Revit.DB.ViewSheet element to get all the placed views on the sheet.
+            var placedViewIds = viewSheet.GetAllPlacedViews();
 
-            return placedViews.Count;
+            //our list to host our views to output
+            var placedViews = new List<global::Revit.Elements.Views.View>();
+
+            //step through each Autodesk.Revit.DB.ElementId element to get it from the current file.
+            foreach (ElementId viewId in placedViewIds)
+            {
+                //get the view as an Autodesk.Revit.DB.View element
+                var internalView = doc.GetElement(viewId) as Autodesk.Revit.DB.View;
+
+                //return the view as a Dynamo Revit.Elements.Views.View element by using ToDSType(false) to cast it
+                placedViews.Add(internalView.ToDSType(false) as global::Revit.Elements.Views.View);
+            }
+
+            //finally, return our newly created list of Revit.Elements.Views.View elements (with clickable green element ids)
+            return placedViews;
         }
     }
 }
