cf5d312f763fdcdedf4979056cb167d322ccc2c8
John <sixtysecondrevit@gmail.com>
2024-01-09 16:26:40 -0700

    Create a new sheet view count node.

---
+++ src/ZeroTouchUnraveled/Revit/Views/Sheet.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Revit.Elements.Views;
+
+
+
+namespace ZeroTouchUnraveled.Revit.Views.Sheet
+{
+    /// <summary>
+    /// Wrapper class for sheets
+    /// </summary>
+    public class Sheet
+    {
+        private Sheet() { }
+
+        /// <summary>
+        /// Get all placed views on the input sheet.
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <returns></returns>
+        public static int GetAllPlacedViews(global::Revit.Elements.Views.Sheet sheet)
+        {
+            //this casts the Dynamo Revit Sheet to a Revit.DB ViewSheet
+            ViewSheet viewSheet = sheet.InternalElement as ViewSheet;
+
+            var placedViews = viewSheet.GetAllPlacedViews();
+
+            return placedViews.Count;
+        }
+    }
+}
